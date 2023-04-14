5eceea19431b75482581e1ff69426ce40b71b293
xKouen <ejya_10@hotmail.com>
2023-04-13 23:51:59 -0700

    Añadidos cambios variados

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name="index"),
+    path('', views.index, name="index"),
     path('recuperarContraseña/', views.recuperarContraseña, name="recuperarContraseña"),
     path('listaAdmin/', views.listaAdmin, name="listaAdmin"),
     path('registroUsuarios/', views.registrarUsuarios, name="registroUsuarios"),
--- myapp/views.py
+++ myapp/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, redirect
-from django.http import HttpRequest
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-def login(request):
-    return render(request, 'login.html')
+def index(request):
+    return HttpResponse('Este es el index')
 def recuperarContraseña(request):
-    return render(request, 'recuperarContraseña.html')
+    return render(request, 'recuperarContraseña')
 def listaUsuarios(request):
-    return render(request, 'listaUsuarios.html')
+    return render(request, 'listaUsuarios')
 def listaAdmin(request):
-    return render(request, 'listaAdmin.html')
+    return render(request, 'listaAdmin')
 def registrarUsuarios(request):
-    return render(request, 'registrarUsuarios.html')
+    return render(request, 'registrarUsuarios')
